c48836d611b45653176de61175e8c02de23698e6
mossaddak <mossaddak15-2413@diu.edu.bd>
2023-06-06 00:49:12 +0600

    app updated

--- main/urls.py
+++ main/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from .views import(
     HomeView,
+    AppView
     
 )
 
 
 urlpatterns = [
-    path('home/', HomeView, name="home")
+    path('home/', HomeView, name="home"),
+    path('app/', AppView, name="app"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import(
+    campaign
+)
 
 # Create your views here.
 @login_required(login_url='login')  
@@ -10,3 +13,14 @@ def HomeView(request):
         
     }
     return render(request, 'home.html', context)
+
+
+@login_required(login_url='login')  
+def AppView(request):
+
+    campaigns = campaign.objects.all() 
+
+    context = {
+        "campaigns":campaigns
+    }
+    return render(request, 'app.html', context)
